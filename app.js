7d9397fec93da984984fbda54d7eb6939b32d45d
Peter <peter.ly@stonybrook.edu>
2018-11-23 21:02:38 -0500

    working on query

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const app = express()
 const port = 3000
 
 var pg = require('pg')
-var connectionString = "postgres://postgres:tenal@localhost/localhost:5432/CSE305MovieDB"
-var pgClient = new pg.Client(connectionString);
-pgClient.connect();
-var query = pgClient.query("SELECT id from Movie where name = 'deadpool'");
+var connectionString_CS = "postgres://postgres:amazingPGtest@localhost/CSE305MovieDB"
+var pgClient = new pg.Client({
+  host: 'localhost',
+  port: 5423,
+  user: 'postgres',
+  password: 'amazingPGtest',
+  connectionString: connectionString_CS,
+});
+pgClient.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+});
+var query = pgClient.query("SELECT id from movies where name = 'deadpool'", (err, res) => {
+  /* etc, etc */
+});
 //query.on("row", function(row,result) {
 //	result.addRow(row);
 	app.get('/', (req, res) => res.send(query))
+	console.log(query);
 //});
 
 
