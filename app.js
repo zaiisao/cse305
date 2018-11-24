986da18a9e5b2f11ba32bdd2695b9a6b999461a3
Jeremy Ahn <me@jeremyahn.com>
2018-11-23 20:10:03 -0500

    testing nodejs integration

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var pg = require('pg')
+var connectionString = "postgres://postgres:tenal@localhost/localhost:5432/CSE305MovieDB"
+var pgClient = new pg.Client(connectionString);
+pgClient.connect();
+var query = pgClient.query("SELECT id from Movie where name = 'deadpool'");
+query.on("row", function(row,result) {
+	result.addRow(row);
+	app.get('/', (req, res) => res.send(result.rows))
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
